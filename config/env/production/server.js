b97280e1223f9e44dabd7af4821d2e2d5bd9bc25
Denisoed <denisoed@gmal.com>
2024-07-02 23:11:32 +0600

    Changed domain

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://api.sklad.cfd',
+  url: 'https://api.sklad.work',
   admin: {
-    url: 'https://dashboard.sklad.cfd/dashboard',
+    url: 'https://dashboard.sklad.work/dashboard',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '12e2ee7cb8c2fcdc713e466fda819b2b'),
     },
